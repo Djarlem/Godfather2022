feb3285da11dc88f6dae64bfeecf61cb22bbca7a
Dadu <55183284+Darnald0@users.noreply.github.com>
2022-09-06 16:00:11 +0200

    Add:
    - Prism spawn

--- Assets/Scripts/Prism.cs
+++ Assets/Scripts/Prism.cs
@@ -5,17 +5,36 @@ using UnityEngine;
 [System.Serializable]
 public struct exit {
     public Vector2 position;
-    public Vector2 direction;
+    public float angle;
 }
 public class Prism : MonoBehaviour
 {
     [SerializeField] private List<exit> exitsList = new List<exit>();
 
+    [SerializeField] private Projectile beamPrefab;
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.F)) {
+            TestSpawn();
+        }
+    }
+
+    private void TestSpawn() {
+        for (int i = 0; i < exitsList.Count; i++) {
+            Spawn(beamPrefab, exitsList[i].angle, exitsList[i].position);
+        }
+    }
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.tag == "beam") {
-            foreach (exit exit in exitsList) {
-                //spawn beam;
+            for (int i = 0; i < exitsList.Count; i++) {
+                Spawn(beamPrefab, exitsList[i].angle, exitsList[i].position);
             }
         }
+    }
+
+    static public void Spawn(Projectile prefab, float direction, Vector3 position) {
+        Projectile newProjectile = Instantiate(prefab);
+        newProjectile.transform.position = position;
+        newProjectile.angleDirection = direction;
     }
 }
