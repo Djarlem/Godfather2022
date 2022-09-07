36545f21577748b3d72ef0b31d35831c842ff920
EstebanNunez <esteban78570@gmail.com>
2022-09-07 10:24:03 +0200

    Add enemies

--- Assets/Scripts/Shooter/Shooter.cs
+++ Assets/Scripts/Shooter/Shooter.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shooter : MonoBehaviour {
+public class Shooter : Singleton<Shooter> {
     [SerializeField] Projectile projectilePrefab;
 
     [SerializeField] float rotation;
