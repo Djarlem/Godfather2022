2872dd6dd8f366f3e20755d9d1dceaca2cd72a3d
EstebanNunez <esteban78570@gmail.com>
2022-09-07 14:50:26 +0200

    Update Enemies

--- Assets/Scripts/Enemies/DiamondEnemy.cs
+++ Assets/Scripts/Enemies/DiamondEnemy.cs
@@ -2,17 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DiamondEnemy : MonoBehaviour
+public class DiamondEnemy : SimpleEnemy
 {
-    // Start is called before the first frame update
-    void Start()
+    // Update is called once per frame
+    protected override void Update()
     {
-        
+        base.Update();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //protected override void FixedUpdate()
+    //{
+      //  base.FixedUpdate();
+    //}
 }
