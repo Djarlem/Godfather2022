e2a9450b30f33adf8f0adea74314d96c058bcc2b
Darnald0 <dudavid1@hotmail.fr>
2022-09-08 14:28:07 +0200

    Add :
    - Game Manager to MainScene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -26,12 +26,6 @@ public class GameManager : MonoBehaviour
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void LoseLife()
     {
         life--;
